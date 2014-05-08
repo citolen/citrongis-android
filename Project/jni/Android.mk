abdd69b95a37796dfbe0ac452adf1017a9714923
mathieu <mat-mathieu@orange.fr>
2014-05-08 18:02:03 +0200

    add proj4 librairie

--- Project/jni/Android.mk
+++ Project/jni/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := CitronGIS
-LOCAL_SRC_FILES := CitronGIS.cpp
+LOCAL_MODULE    		:= CitronGIS
+LOCAL_SRC_FILES 		:= CitronGIS.cpp
+LOCAL_STATIC_LIBRARIES	:= /usr/local/lib/libproj.so
 
 include $(BUILD_SHARED_LIBRARY)
