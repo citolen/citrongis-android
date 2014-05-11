8e3fa45d377f0d48c632462a513ee2fbaa08c6d6
Romain <romain@romain-HP-EliteBook-8540p.(none)>
2014-05-11 18:20:22 +0200

    CoordinatesHelper class

---
+++ Class/CoordinatesHelper.h
@@ -0,0 +1,34 @@
+/*
+ * Point.h
+ *
+ *  Created on: May 11, 2014
+ *      Author: romain
+ */
+
+#ifndef COORDINATESHELPER_H_
+#define COORDINATESHELPER_H_
+
+#include	"Point.h"
+#include	<proj_api.h>
+#include	<string>
+
+namespace C
+{
+  namespace Helpers
+  {
+    class CoordinatesHelper {      
+      
+    public:
+      
+      /* Coplien Form*/
+      CoordinatesHelper();
+      CoordinatesHelper(const CoordinatesHelper &);
+      CoordinatesHelper	&operator=(const CoordinatesHelper &);
+      virtual	~CoordinatesHelper();
+
+      C::Geometry::Point	TransformTo(const C::Geometry::Point pt, const projPJ CRS);
+      C::Geometry::Point	TransformTo(const C::Geometry::Point pt, const std::string proj);
+    };
+  }
+}
+#endif /* COORDINATESHELPER_H_ */
