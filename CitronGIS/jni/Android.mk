a40c4445df8798038f6bfacae6897c1caae7a2b2
mathieu <mat-mathieu@orange.fr>
2014-05-26 21:50:33 +0200

    add proj4 librairy

--- CitronGIS/jni/Android.mk
+++ CitronGIS/jni/Android.mk
@@ -9,15 +9,13 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/Headers/
 LOCAL_C_INCLUDES += /usr/local/include/
 
 ## Sources files
-LOCAL_SRC_FILES := Sources/main.cpp
+LOCAL_SRC_FILES :=	Sources/main.cpp \
+					Sources/Geometry/Point.cpp
 
 ## Libraries
-LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM -L$(LOCAL_PATH)/libs -lproj
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 LOCAL_STATIC_LIBRARIES += clibs
-LOCAL_STATIC_LIBRARIES += /usr/local/lib/libproj
-
-
 
 include $(BUILD_SHARED_LIBRARY)
 
