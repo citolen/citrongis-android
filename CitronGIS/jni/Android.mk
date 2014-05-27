b61ce1b2a42d55aad48ecb4c0aafe09c4959b79b
mathieu <mat-mathieu@orange.fr>
2014-05-27 21:52:52 +0200

    up

--- CitronGIS/jni/Android.mk
+++ CitronGIS/jni/Android.mk
@@ -1,7 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE:= proj
+LOCAL_SRC_FILES := libs/libproj.so
+include $(PREBUILT_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE    := CitronGIS
 
 ## Includes path
@@ -9,14 +13,16 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/Headers/
 LOCAL_C_INCLUDES += /usr/local/include/
 
 ## Sources files
-LOCAL_SRC_FILES :=	Sources/main.cpp \
-					Sources/Geometry/Point.cpp
+LOCAL_SRC_FILES :=	Sources/main.cpp
+			##		Sources/Geometry/Point.cpp
 
 ## Libraries
-LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM -L$(LOCAL_PATH)/libs -lproj
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM -Wl,-rpath=$(LOCAL_PATH)/libs
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 LOCAL_STATIC_LIBRARIES += clibs
-
+LOCAL_SHARED_LIBRARIES := proj
+##LOCAL_LDFLAGS := -Wl,-soname,. -Wl,-soname,/data/libs
+##LOCAL_LDFLAGS := -Wl,-soname,$(LOCAL_PATH)/libs
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/native_app_glue)
