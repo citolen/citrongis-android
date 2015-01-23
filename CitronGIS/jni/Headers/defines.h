58d14be9ee1bb726c0be4edd614edec83c564757
vanwesm <mathieu.vanwesterlaak@epitech.eu>
2015-01-23 11:33:47 -0800

    clean

--- CitronGIS/jni/Headers/defines.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DEFINES_H__
-#define __DEFINES_H__
-
-#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "native-activity", __VA_ARGS__))
-#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "native-activity", __VA_ARGS__))
-#define LOGA(...) ((void)__android_log_print(ANDROID_LOG_FATAL, "native-activity", __VA_ARGS__))
-
-#endif
